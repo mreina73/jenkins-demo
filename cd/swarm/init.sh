ba2ca2cad31ff95b6e9a73a6a836a33739947085
Mike Reina <mike.reina@gmail.com>
2020-05-06 11:34:29 -0400

    Update init.sh

--- cd/swarm/init.sh
+++ cd/swarm/init.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 
 docker network create db --driver overlay
-docker service create --name db --network db -e MYSQL_ROOT_PASSWORD=test mysql:5.7.17
+docker service create --name db --network db -e MYSQL_ROOT_PASSWORD=test mariadb
 docker secret create db-alpha db-alpha-secret.json
 docker secret create db-beta db-beta-secret.json
 docker secret create db-prod db-prod-secret.json
